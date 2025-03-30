96175a6f566d4b7310cebce5596f0803675a2c35
DangDinhKhoa <dinhkhoa090@gmail.com>
2025-03-30 23:29:52 +0700

    sigup, login, logout

--- website_data/laravel_12_base/routes/web.php
+++ website_data/laravel_12_base/routes/web.php
@@ -1,6 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+
+Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login_post');
+
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware('auth');
+
 
 Route::get('/', function () {
     return view('welcome');
